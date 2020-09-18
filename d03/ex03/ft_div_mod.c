a078eb49f65c9f5f304bc03b238320b9dc50e403
Leesa Flint <lflint@un-l3.kzn.21-school.ru>
2020-09-18 11:54:26 +0300

    pic for 3rd day

--- d03/ex03/ft_div_mod.c
+++ d03/ex03/ft_div_mod.c
@@ -1,16 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lflint <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/17 20:22:03 by lflint            #+#    #+#             */
-/*   Updated: 2020/09/18 10:25:07 by lflint           ###   ########.fr       */
+/*   Created: 2020/09/18 11:01:40 by lflint            #+#    #+#             */
+/*   Updated: 2020/09/18 11:41:24 by lflint           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-void		ft_ft(int *nbr)
+#include <stdio.h>
+void		ft_div_mod(int a, int b, int *div, int *mod)
 {
-	*nbr = 42;
+	*div = a / b;
+	*mod = a % b;
+}
+
+int		main(void)
+{
+	int a;
+	int b;
+
+	a = 47;
+	b = 10;
+
+	int div = 0;
+	int *ptrd;
+	ptrd = &div;
+	int *ptrm;
+	int mod = 0;
+	ptrm = &mod;
+
+	ft_div_mod(a, b, ptrd, ptrm);
+	printf("%d\n", *ptrd);
+	printf("%d", *ptrm);
+
 }
